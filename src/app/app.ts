4ddb9839bbdd17a441557ff5fa690916a3ff461a
hFire24 <51500563+hFire24@users.noreply.github.com>
2026-01-27 12:36:54 -0500

    Mobile devices now use Archivo Black in most places if it were to use Arial Black

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Character } from './services/character.service';
 import { Mood } from './services/mood.service';
 import { RouterOutlet } from '@angular/router';
+import { DeviceService } from './services/device.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  private deviceService = inject(DeviceService);
+  
   showMoreCharacters = false;
   selectedCharacter: Character | null = null;
   selectedMood: Mood | null = null;
+
+  ngOnInit() {
+    // Add iOS class to body if on iOS device
+    if (this.deviceService.isIOS()) {
+      document.body.classList.add('ios-device');
+    }
+  }
 
   toggleCharacters() {
     this.showMoreCharacters = !this.showMoreCharacters;
